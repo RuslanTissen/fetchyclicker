69c155bc9eb36f3f3672700b8616a0c69df01ad2
Ruslan Tissen <rthyssen73@gmail.com>
2024-11-16 11:17:42 +0100

    in progress 16.11.24

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Search() {
     const [searchResults, setSearchResults] = useState(null)
-    
-    const url = "https://api.github.com/search/repositories?q=react&per_page=100&page=1"
 
-    fetch(url)
-        .then(response => {
-            console.log(response)
-            return response.json()
-        })
-        .then(results => {
-            setSearchResults(results.items)
-        })
+    useEffect(function() {
+      const url = "https://api.github.com/search/repositories?q=react&per_page=100&page=1"
+      fetch(url)
+          .then(response => {
+              console.log(response)
+              return response.json()
+          })
+          .then(results => {
+              setSearchResults(results.items)
+          })
+    }, [])
 
 
   return (
